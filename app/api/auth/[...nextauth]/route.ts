02136469b124479df1c1db6490412fdf42df6605
Roger William <rogmtsi.df@gmail.com>
2024-05-05 11:14:57 -0300

    chore: add next-auth setup

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import PrismaAdapter from "@auth/prisma-adapter";
+import { db } from "@/app/_lib/prisma";
+import { Adapter } from "next-auth/adapters";
+
+const handler = NextAuth({
+  adapter: PrismaAdapter(db) as Adapter,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+});
